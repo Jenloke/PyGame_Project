bec74dd9cf157a1bcbd5d7dc2db7b0f289891dc0
Jenloke <mjenloke@gmail.com>
2022-05-14 22:09:15 +0800

    test parallax bg

--- main.py
+++ main.py
@@ -12,6 +12,31 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 level = Level(level_map,screen)
 
+#define game variables
+scroll = 0
+
+ground_image = pygame.image.load("parallax/ground.png").convert_alpha()
+ground_width = ground_image.get_width()
+ground_height = ground_image.get_height()
+
+bg_images = []
+for i in range(1, 6):
+  bg_image = pygame.image.load(f"parallax/plx-{i}.png").convert_alpha()
+  bg_images.append(bg_image)
+bg_width = bg_images[0].get_width()
+
+def draw_bg():
+  for x in range(5):
+    speed = 1
+    for i in bg_images:
+      screen.blit(i, ((x * bg_width) - scroll * speed, 0))
+      speed += 0.2
+
+def draw_ground():
+  for x in range(15):
+    screen.blit(ground_image, ((x * ground_width) - scroll * 2.5, screen_height - ground_height))
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,6 +44,17 @@ while True:
             sys.exit()
 
     screen.fill('black')
+    #parallax bg
+    draw_bg()
+    draw_ground()
+    
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and scroll > 0:
+        scroll -= 5
+    if key[pygame.K_RIGHT] and scroll < 1200:
+        scroll += 5
+
+    
     level.run()
 
     pygame.display.update()
